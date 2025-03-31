980dc9a5f7a62137283cd0952ca9648bf271e851
JOS28334IaHm <content.ketone0m@icloud.com>
2025-03-31 06:17:55 +0100

    Add a user profile editing feature in the file

--- jCm/UeMQ/folh-n/uXLqIM/student-centered-learning_pLSZ_1743103018.md.py
+++ jCm/UeMQ/folh-n/uXLqIM/student-centered-learning_pLSZ_1743103018.md.py
@@ -12,5 +12,4 @@ dataset = pd.read_csv('Pre_AB186_Circuit_raw.csv')  # Change the Csv name here t
 dataset2 = dataset[['Right_Shank_Ax','Right_Shank_Ay','Right_Shank_Az','Right_Shank_Gx','Right_Shank_Gy','Right_Shank_Gz','Right_Thigh_Ax','Right_Thigh_Ay','Right_Thigh_Az','Right_Thigh_Gx','Right_Thigh_Gy','Right_Thigh_Gz','Mode']].round(14)
 #%%
 dataset2.to_csv('AB186_Circuit_raw.csv', index = False)  # chane output name to be not the same as input to not over write it
-#%%
 dataset3 = pd.read_csv('AB186_Circuit_raw.csv') # Useless Line for Viualize not more
